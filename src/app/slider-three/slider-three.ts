9fca4fb8be8e37625b54aa06c4c928b7d5e7b0e8
mhumayon054 <mhumayon054@gmail.com>
2025-12-07 21:51:36 +0500

    spotlight links

--- src/app/slider-three/slider-three.ts
+++ src/app/slider-three/slider-three.ts
@@ -10,27 +10,27 @@ import { CommonModule } from '@angular/common';
 export class SliderThree {
   images = [
     {
-      url: 'assets/Models/Thaila Shine.jpeg',
+      url: 'https://ik.imagekit.io/5sbqruotr/spotlightImages/Thal%C3%ADa.jpeg?updatedAt=1765120329266',
       text: 'Thalia shine while accepting the President’s Merit Award at the  20th Annual Latin Grammys in a Yas Couture Gown'
     },
     {
-      url: 'assets/Models/Amyra Dastur.jpeg',
+      url: 'https://ik.imagekit.io/5sbqruotr/spotlightImages/Amyra%20Dastur.jpeg?updatedAt=1765120326927',
       text: 'Amyra Dastur looking flawless in a Yas Couture See through mirror beaded gown.'
     },
     {
-      url: 'assets/Models/Kety Perry.jpeg',
+      url: 'https://ik.imagekit.io/5sbqruotr/spotlightImages/Kety%20Perry.jpeg',
       text: 'Katty Perry Shines in a Yas Couture silver embellished catsuit dress for the Video Music Awards'
     },
     {
-      url: 'assets/Models/Gwen Stephani.jpeg',
+      url: 'https://ik.imagekit.io/5sbqruotr/spotlightImages/Gwen%20Stephani.jpeg',
       text: 'The Beauty Icon Gwen Stefani glowing in a Yas Couture golden  bodysuit for GXVE Beauty.'
     },
     {
-      url: 'assets/Models/Golden Godess.jpeg',
+      url: 'https://ik.imagekit.io/5sbqruotr/spotlightImages/Golden%20Godess.jpeg',
       text: 'Golden Goddess Paris Hilton looking absolutely  stunning in a Golden beaded gown from Yas Couture'
     },
     {
-      url: 'assets/48-1.jpeg',
+      url: 'https://ik.imagekit.io/5sbqruotr/spotlightImages/48-1.jpeg',
       text: 'Jennifer Lopez graces the stage for the Robin Hood Foundation  in New York wearing a Yas Couture costume.'
     },
   ];
